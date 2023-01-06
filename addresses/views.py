d939a1997f3c551057a10039b6e203cbf2eb12f0
lucien <zimwolfgang24@gmail.com>
2023-01-06 12:32:12 -0400

    feat: atualizacao urls settings

--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import AddressSerializer
+from .models import Address
+
+class AddressView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    serializer_class = AddressSerializer
+    queryset =  Address.objects.all()
 
 # Create your views here.
