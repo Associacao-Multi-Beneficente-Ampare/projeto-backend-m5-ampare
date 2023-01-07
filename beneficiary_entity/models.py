4762ad63b1afea09c90f687713ee0af993cb6adc
Allan da Mota Salatino <allan.smota@gmail.com>
2023-01-07 11:07:33 -0300

    conflitos resolvidos

--- beneficiary_entity/models.py
+++ beneficiary_entity/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from beneficiary_entity import CampaignsProjects
 import uuid
 
 
--- campaigns_projects/views.py
+++ campaigns_projects/views.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import generics
 
+
 class CampaignsProjectsView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
