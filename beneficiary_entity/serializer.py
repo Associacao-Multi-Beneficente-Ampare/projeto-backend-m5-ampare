c7f07c1626432857b78120684d4dda70087099a3
Kátya <katyakeila@gmail.com>
2023-01-09 16:30:36 -0300

    correções users login email

--- beneficiary_entity/serializer.py
+++ beneficiary_entity/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import BeneficiaryEntity
+
+
 class BeneficiaryEntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = BeneficiaryEntity
@@ -12,12 +14,17 @@ class BeneficiaryEntitySerializer(serializers.ModelSerializer):
             "email",
             "cnpj",
         ]
+
     email = serializers.EmailField(
         validators=[UniqueValidator(queryset=BeneficiaryEntity.objects.all())],
     )
+
     def create(self, validated_data: dict) -> BeneficiaryEntity:
         return BeneficiaryEntity.objects.create(**validated_data)
-    def update(self, instance: BeneficiaryEntity, validated_data: dict) -> BeneficiaryEntity:
+
+    def update(
+        self, instance: BeneficiaryEntity, validated_data: dict
+    ) -> BeneficiaryEntity:
         for key, value in validated_data.items():
             setattr(instance, key, value)
         instance.save()
