171c3da51a29cb25962f0b58890a18f061d42913
lucien <zimwolfgang24@gmail.com>
2023-01-10 08:53:31 -0400

    feat: configuracoes deploy

--- _ampare/settings.py
+++ _ampare/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://web-production-1446.up.railway.app/", "0.0.0.0"]
 
 
 # Application definition
