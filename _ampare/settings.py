2cc2a3a5d55832efb79991897c2d99976ce6e3b3
Allan da Mota Salatino <98770586+AllanSalatino@users.noreply.github.com>
2023-01-11 19:30:29 -0300

    Update settings.py

--- _ampare/settings.py
+++ _ampare/settings.py
@@ -128,9 +128,9 @@ if DATABASE_URL:
     db_deploy = dj_database_url.config(
         default=DATABASE_URL)
     DATABASES['default'].update(db_deploy)
-    DEBUG = True
+    DEBUG = False
 
-if DEBUG:
+if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
